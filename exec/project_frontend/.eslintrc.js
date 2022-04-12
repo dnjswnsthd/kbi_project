2fc4ddbe7d368f478e4acfe8b2a24c105c2d9473
dnjswnsthd <dnjswnsthd@naver.com>
2022-04-12 19:00:51 +0900

    Add eslint-setting

--- exec/project_frontend/.eslintrc.js
+++ exec/project_frontend/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
+    'linebreak-style': 0,
+	  'prettier/prettier': ['error', { endOfLine: 'auto'} ],
   },
-  'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
-	'prettier/prettier': ['error', { endOfLine: 'auto'} ],
 };
