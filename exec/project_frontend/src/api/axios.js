5a0a06fc87043570bbee5d8754e6c50fc99da0e8
dnjswnsthd <dnjswnsthd@naver.com>
2022-04-29 21:26:24 +0900

    fix

--- exec/project_frontend/src/api/axios.js
+++ exec/project_frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // axios
 export default axios.create({
-  baseURL: "http://localhost:8000/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "http://kingofinvestment.shop/api/",
   //baseURL: "https://k4d109.p.ssafy.io/api/", // baseUrl
   headers: {
     "Content-type": "application/json",
