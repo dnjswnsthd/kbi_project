cf6399583a86ef5ee5140aa3a676ce31723e9a34
dnjswnsthd <dnjswnsthd@naver.com>
2022-05-05 19:39:24 +0900

    fix frontend

--- exec/project_frontend/src/api/move.js
+++ exec/project_frontend/src/api/move.js
@@ -1,26 +1,21 @@
 import ROUTER from "@/router";
 
-/* 시작 페이지로 이동 */
 function moveStart() {
   return ROUTER.push({ name: "Start" });
 }
 
-/* 로그인 페이지로 이동 */
 function moveLogin() {
   return ROUTER.push({ name: "Login" });
 }
 
-/* 회원가입 페이지로 이동 */
 function moveSignUp() {
   return ROUTER.push({ name: "SignUp" });
 }
 
-/* 메인 페이지로 이동 */
 function moveMain() {
   return ROUTER.push({ name: "Main" });
 }
 
-/* 마이 페이지로 이동 */
 function moveMyPage() {
   return ROUTER.push({ name: "MyPage" });
 }
