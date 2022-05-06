aecafa712b3c55f2f319eb54dc4bdcad8a5d9e7e
dnjswnsthd <dnjswnsthd@naver.com>
2022-05-06 13:14:00 +0900

    fix

--- exec/project_frontend/.eslintrc.js
+++ exec/project_frontend/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
@@ -10,8 +11,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
-    'linebreak-style': 0,
-	  'prettier/prettier': ['error', { endOfLine: 'auto'} ],
+    "linebreak-style": [
+      "error",
+      require("os").EOL === "\r\n" ? "windows" : "unix",
+    ],
+    // eslint-disable-next-line no-dupe-keys
+    "linebreak-style": 0,
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
 };
